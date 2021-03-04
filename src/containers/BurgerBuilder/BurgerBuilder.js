62b4e6083d6b7d58645f10ae97797f9da00b23ae
vandoan <vdoan09@gmail.com>
2021-03-03 21:26:38 -0800

    181

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES = {
     bacon:  0.7,
     cheese: 0.4,
@@ -65,7 +65,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Onwards');
+        // alert('Onwards');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Wing Dinger',
+                address: {
+                    street: '1 Test St.',
+                    zipCode: '43424',
+                    county: 'Germany'
+                },
+                email: 'test@test.com',
+
+            },
+            deliveryMethod: 'Snail'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     removeIngredientHandler = (type => {
