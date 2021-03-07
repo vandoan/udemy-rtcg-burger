3f010a4d7097c9daea20d83658aa5f55b4ff5ac5
vandoan <vdoan09@gmail.com>
2021-03-06 20:53:35 -0800

    Lesson 225

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,27 +73,36 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('Onwards');
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Wing Dinger',
-                address: {
-                    street: '1 Test St.',
-                    zipCode: '43424',
-                    county: 'Germany'
-                },
-                email: 'test@test.com',
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Wing Dinger',
+        //         address: {
+        //             street: '1 Test St.',
+        //             zipCode: '43424',
+        //             county: 'Germany'
+        //         },
+        //         email: 'test@test.com',
 
-            },
-            deliveryMethod: 'Snail'
+        //     },
+        //     deliveryMethod: 'Snail'
+        // }
+        // axios.post('/orders.s', order)
+        //     .then(response => this.setState({loading: false,  purchasing: false}))
+        //     .catch(error => 
+        //         this.setState({loading: false, purchasing: false} )
+        //         );
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.s', order)
-            .then(response => this.setState({loading: false,  purchasing: false}))
-            .catch(error => 
-                this.setState({loading: false, purchasing: false} )
-                );
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     removeIngredientHandler = (type => {
